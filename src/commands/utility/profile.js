29b270117a4e52bebf8dcaed502e254d33716fa1
mintea <zetari.gen@outlook.com>
2020-01-23 17:48:04 -0500

    v0.2 a3U
    update on profiles

--- src/commands/utility/profile.js
+++ src/commands/utility/profile.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 const randomColor = require('randomcolor')
 
 class profile extends Command {
-  constructor(...args) {
+  constructor (...args) {
     super(...args, {
       name: 'profile',
       cooldown: 10,
@@ -14,12 +14,16 @@ class profile extends Command {
     })
   }
 
-  async handle({ args, client, msg }, responder) {
+  async handle ({ args, client, msg }, responder) {
     const color = parseInt(randomColor().replace(/#/gi, '0x'))
     const user = args.user
-    const sUserXP = db.fetch(`${msg.channel.guild.id}.profiles.${msg.author.id}.xp`)
+    const sUserXP = db.fetch(
+      `${msg.channel.guild.id}.profiles.${msg.author.id}.xp`
+    )
     const gUserXP = db.fetch(`global.profiles.${msg.author.id}.xp`)
-    const sUserLevel = db.fetch(`${msg.channel.guild.id}.profiles.${msg.author.id}.level`)
+    const sUserLevel = db.fetch(
+      `${msg.channel.guild.id}.profiles.${msg.author.id}.level`
+    )
     const gUserLevel = db.fetch(`global.profiles.${msg.author.id}.level`)
     const gUserDesc = db.fetch(`global.profiles.${msg.author.id}.desc`)
     if (sUserXP == null) db.set(`${msg.channel.guild.id}.profiles.${msg.author.id}.xp`, 1)
@@ -29,30 +33,30 @@ class profile extends Command {
     if (gUserDesc == null) db.set(`global.profiles.${msg.author.id}.desc`, 'A Discord user')
     if (!user) {
       let embed = {
-        'embed': {
-          'color': color,
-          'timestamp': new Date(),
-          'description': gUserDesc,
-          'footer': {
-            'icon_url': msg.author.avatarURL,
-            'text': `Requested by ${msg.author.username}`
+        embed: {
+          color: color,
+          timestamp: new Date(),
+          description: gUserDesc,
+          footer: {
+            icon_url: msg.author.avatarURL,
+            text: `Requested by ${msg.author.username}`
           },
-          'author': {
-            'name': `${msg.author.username}'s Profile`
+          author: {
+            name: `${msg.author.username}'s Profile`
           },
-          'thumbnail': {
-            'url': `${msg.author.avatarURL}`
+          thumbnail: {
+            url: `${msg.author.avatarURL}`
           },
-          'fields': [
+          fields: [
             {
-              'name': 'User Information',
-              'value': `Level: ${sUserLevel} (${sUserXP})`,
-              'inline': true
+              name: 'User Information',
+              value: `Level: ${sUserLevel} (${sUserXP})`,
+              inline: true
             },
             {
-              'name': 'Global User Information',
-              'value': `Level: ${gUserLevel} (${gUserXP})`,
-              'inline': true
+              name: 'Global User Information',
+              value: `Level: ${gUserLevel} (${gUserXP})`,
+              inline: true
             }
           ]
         }
